b17d5247980575673c7c20aa483c15320a3aa990
‘NateMietk’ <natemietkiewicz@gmail.com>
2019-02-26 12:55:58 -0700

    remove cart and roc plots - moved and expanded in another script

--- src/R/random_forest_model.R
+++ src/R/random_forest_model.R
@@ -90,7 +90,7 @@ if(length(model_list) != 20) {
       model_ranger <- caret::train(ignition ~ .,
                                    data = train,
                                    method = "ranger",
-                                   num.threads = parallel::detectCores(), # for parallel processing
+                                   num.threads = parallel::detectCores(),
                                    metric = 'ROC',
                                    weights = model_weights,
                                    trControl = training_parameters,
@@ -105,93 +105,21 @@ if(length(model_list) != 20) {
     
     if(!file.exists(file.path(model_dir, paste0('importance_pval_', i, '.rds')))) {
       print(paste0('Computing variable importance p-value for ', i))
-      model_importance_pvalues <- importance_pvalues(model_ranger$finalModel, method = "altmann", formula = ignition ~ ., data = train)
+      model_importance_pvalues <- importance_pvalues(model_ranger$finalModel, 
+                                                     method = "altmann", formula = ignition ~ ., data = train)
       write_rds(model_importance_pvalues, file.path(model_dir, paste0('importance_pval_', i, '.rds'))) 
       system(paste0('aws s3 sync ', model_dir, ' ', s3_proc_models))
     }
     
-    if(!file.exists(file.path(model_dir, paste0('confusion_matrix_', i, '.rds')))) {
+    if(!file.exists(file.path(model_dir, paste0('confusion_ranger_', i, '.rds')))) {
       # predict the outcome on a test set
       ranger_pred <- predict(model_ranger, test)
+      write_rds(predict_rpart, file.path(model_dir, 'prediction_ranger', i, '.rds'))
       # compare predicted outcome and true outcome
       confusion_matrix <- confusionMatrix(ranger_pred, test$ignition)
       
-      write_rds(confusion_matrix, file.path(model_dir, paste0('confusion_matrix_', i, '.rds'))) 
+      write_rds(confusion_matrix, file.path(model_dir, paste0('confusion_ranger_', i, '.rds'))) 
       system(paste0('aws s3 sync ', model_dir, ' ', s3_proc_models))
     }
   }
-} else {
-  all_models <- lapply(list.files(file.path(model_dir), pattern = 'model_ranger_', full.names = TRUE),
-                       function(x)read_rds(x))    
-  all_importance_pval <- all_models <- lapply(list.files(file.path(model_dir), pattern = 'importance_pval_', full.names = TRUE),
-                                              function(x)read_rds(x))    
-}
-
-
-
-
-
-
-# Calculate the p value for the variable imporances 
-importance_pvalues(model_ranger_ne$finalModel, method = "altmann", formula = ignition ~ ., data = train)
-
-# Plot the ROC evaluation of the model 
-selectedIndices <- model_ranger_ne$pred$mtry == tuned_ranger_ne$recommended.pars$mtry
-
-model_ranger_roc <- model_ranger_ne$pred[selectedIndices, ] %>%
-  mutate(bool = ifelse(obs == 'Human', 1, 0)) %>%
-  ggplot(aes(m = Human, d = bool)) + 
-  geom_roc(n.cuts=0) + 
-  coord_equal() +
-  style_roc() 
-model_ranger_roc <- model_ranger_roc +
-  annotate("text", x=0.75, y=0.25, label=paste("AUC =", round((calc_auc(model_ranger_roc))$AUC, 4)))
-model_ranger_roc
-
-plot(varImp(model_ranger_ne))
-
-varImp(model_ranger_ne)$importance %>% 
-  as.data.frame() %>%
-  rownames_to_column() %>%
-  arrange(desc(Overall)) %>%
-  mutate(variable = forcats::fct_inorder(rowname )) %>%
-  slice(1:15) %>%
-  ggplot(aes(x=reorder(variable,Overall), y=Overall)) + 
-  geom_bar(stat="identity", position="dodge", width = 0.01, fill = 'black') + 
-  geom_point() +
-  coord_flip() +
-  ylab("Variable Importance") +
-  xlab("") +
-  ggtitle("Information Value Summary") +
-  guides(fill=F) +
-  theme_pub()
-
-# predict the outcome on a test set
-ranger_pred <- predict(model_ranger_ne, test)
-# compare predicted outcome and true outcome
-confusionMatrix(ranger_pred, test$ignition)
-
-# plot rpart of top 15 vars
-vars <- varImp(model_ranger_ne)$importance %>% 
-  as.data.frame() %>%
-  rownames_to_column() %>%
-  arrange(desc(Overall)) %>%
-  mutate(variable = forcats::fct_inorder(rowname )) %>%
-  slice(1:10) %>%
-  dplyr::select(variable) %>%
-  pull(., variable) %>%
-  droplevels()
-test <- paste(vars, collapse = ' ')
-
-model_rpart_us <- caret::train(ignition ~ tmmx_mean_lag_0+vector_primary_rds_distance+vector_tertiary_rds_distance+
-                                 vector_secondary_rds_distance+vector_railroad_distance+
-                                 state+ffwi_mean_lag_0+def_mean_lag_0++vpd_min_3_month+def_mean_3_month+vpd_min_6_month,
-                               data = train,
-                               method = "rpart",
-                               weights = model_weights,
-                               trControl = training_parameters,
-                               tuneLength = 15)
-plot(model_rpart_us$finalModel)
-text(model_rpart_us$finalModel)
-library(rattle)
-fancyRpartPlot(model_rpart_us$finalModel)
+} 
