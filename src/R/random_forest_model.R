a92ffbf9630a214c04956df9af89a74c58defb7a
‘NateMietk’ <natemietkiewicz@gmail.com>
2019-06-28 19:01:00 +0000

    changed so the model and pval is computed by janitza

--- src/R/random_forest_model.R
+++ src/R/random_forest_model.R
@@ -19,6 +19,7 @@ fpa_all_vars <- read_rds(file.path(extraction_dir, 'fpa_all_vars.rds')) %>%
   mutate_if(is.character, as.factor) %>%
   mutate(state = as.factor(toupper(state)))
 
+
 model_list <- list.files(file.path(model_dir), pattern = 'model_ranger_', full.names = TRUE)
 if(length(model_list) != 20) {
   for(i in unique(fpa_all_vars$na_l2name)) {
@@ -53,30 +54,30 @@ if(length(model_list) != 20) {
     set.seed(432)
     mlr_tasked = makeClassifTask(data = train, target = "ignition", weights = model_weights)
     
-    if(!file.exists(file.path(model_dir, 'tuneRanger_time.rds'))) {
-      # Time estimation
-      tuneRanger_time <- lubridate::seconds_to_period(estimateTimeTuneRanger(task = mlr_tasked, num.trees = 1000,
-                                                                             num.threads = parallel::detectCores(), iters= 10))
-      l2_ecoregion <- i
-      time_df = rbind(time_df, data.frame(tuneRanger_time, l2_ecoregion))
-      write_rds(time_df, file.path(model_dir, 'tuneRanger_time.rds'))
-    }
+    # if(!file.exists(file.path(model_dir, 'tuneRanger_time.rds'))) {
+    #   # Time estimation
+    #   tuneRanger_time <- lubridate::seconds_to_period(estimateTimeTuneRanger(task = mlr_tasked, num.trees = 1000,
+    #                                                                          num.threads = parallel::detectCores(), iters= 10))
+    #   l2_ecoregion <- i
+    #   time_df = rbind(time_df, data.frame(tuneRanger_time, l2_ecoregion))
+    #   write_rds(time_df, file.path(model_dir, 'tuneRanger_time.rds'))
+    # }
     
     # Tuning process for the lower 48 states
-    if(!file.exists(file.path(model_dir, paste0('tuned_ranger_', i, '.rds')))) {
+    if(!file.exists(file.path(tuned_dir, paste0('tuned_ranger_', i, '.rds')))) {
       set.seed(432)
       mlr_tasked = makeClassifTask(data = train, target = "ignition", weights = model_weights)
       
       tuned_ranger <- tuneRanger(mlr_tasked, measure = list(multiclass.brier), num.trees = 1000, time.budget = 36000,
                                  num.threads = parallel::detectCores(), build.final.model = FALSE)
-      tuned_ranger <- write_rds(tuned_ranger, file.path(model_dir, paste0('tuned_ranger_', i, '.rds')))
+      tuned_ranger <- write_rds(tuned_ranger, file.path(tuned_dir, paste0('tuned_ranger_', i, '.rds')))
       system(paste0('aws s3 sync ', model_dir, ' ', s3_proc_models))
     } else {
-      tuned_ranger <- read_rds(file.path(model_dir, paste0('tuned_ranger_', i, '.rds')))
+      tuned_ranger <- read_rds(file.path(tuned_dir, paste0('tuned_ranger_', i, '.rds')))
     }
     
     # Model with the tuned hyperparameter from tuneRanger
-    if(!file.exists(file.path(model_dir, paste0('model_ranger_', i, '.rds')))) {
+    if(!file.exists(file.path(janitza_dir, paste0('model_ranger_', i, '.rds')))) {
       training_parameters <- trainControl(summaryFunction = twoClassSummary,
                                           classProbs = TRUE,
                                           verboseIter  = TRUE,
@@ -95,31 +96,32 @@ if(length(model_list) != 20) {
                                    weights = model_weights,
                                    trControl = training_parameters,
                                    tuneGrid = tuning_grid,
-                                   num.trees = 2500,
-                                   importance = 'permutation')
-      write_rds(model_ranger, file.path(model_dir, paste0('model_ranger_', i, '.rds'))) 
+                                   num.trees = 1000,
+                                   importance = 'impurity_corrected')
+      write_rds(model_ranger, file.path(janitza_dir, paste0('model_ranger_', i, '.rds'))) 
       system(paste0('aws s3 sync ', model_dir, ' ', s3_proc_models))
     } else {
-      model_ranger <- read_rds(file.path(model_dir, paste0('model_ranger_', i, '.rds')))
+      model_ranger <- read_rds(file.path(janitza_dir, paste0('model_ranger_', i, '.rds')))
     }
     
-    if(!file.exists(file.path(model_dir, paste0('importance_pval_', i, '.rds')))) {
-      print(paste0('Computing variable importance p-value for ', i))
-      model_importance_pvalues <- importance_pvalues(model_ranger$finalModel, 
-                                                     method = "altmann", formula = ignition ~ ., data = train)
-      write_rds(model_importance_pvalues, file.path(model_dir, paste0('importance_pval_', i, '.rds'))) 
-      system(paste0('aws s3 sync ', model_dir, ' ', s3_proc_models))
-    }
+    if(!file.exists(file.path(janitza_dir, paste0('importance_pval_', i, '.rds')))) {
+        print(paste0('Computing variable importance p-value for ', i))
+        model_importance_pvalues <- importance_pvalues(model_ranger$finalModel, 
+                                                       method = "janitza")
+        write_rds(model_importance_pvalues, file.path(janitza_dir, paste0('importance_pval_', i, '.rds'))) 
+        system(paste0('aws s3 sync ', model_dir, ' ', s3_proc_models))
+      } else {
+        print(paste0('Skipping variable importance p-value for ', i))
+        }
     
-    if(!file.exists(file.path(model_dir, paste0('confusion_ranger_', i, '.rds')))) {
+    if(!file.exists(file.path(janitza_dir, paste0('confusion_ranger_', i, '.rds')))) {
       # predict the outcome on a test set
       ranger_pred <- predict(model_ranger, test)
-      write_rds(predict_rpart, file.path(model_dir, 'prediction_ranger', i, '.rds'))
       # compare predicted outcome and true outcome
       confusion_matrix <- confusionMatrix(ranger_pred, test$ignition)
       
-      write_rds(confusion_matrix, file.path(model_dir, paste0('confusion_ranger_', i, '.rds'))) 
+      write_rds(confusion_matrix, file.path(janitza_dir, paste0('confusion_ranger_', i, '.rds'))) 
       system(paste0('aws s3 sync ', model_dir, ' ', s3_proc_models))
     }
   }
-} 
+}
