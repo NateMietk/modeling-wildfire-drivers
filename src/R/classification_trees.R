77f65e1eb27b24fcfc5672ea03f04e9ee6824339
‘NateMietk’ <natemietkiewicz@gmail.com>
2019-08-29 18:41:38 +0000

    updated output naming convention. added a check for owner_descr variable

--- src/R/classification_trees.R
+++ src/R/classification_trees.R
@@ -1,14 +1,15 @@
 # plot rpart of top 15 vars
-for(i in unique(top_15_sig_importance$lvl2_ecoregion)) {
+for(i in unique(top_15_sig_importance$na_l2name)) {
 
   set.seed(224)
   subset <- fpa_all_vars %>% 
-    filter(na_l2name == i ) %>%
+    filter(na_l2name == gsub(' ', '-', i)) %>%
     droplevels() %>%
     select_if(~ nlevels(.) > 1 | is.numeric(.))
   
   train <- subset %>%
     dplyr::sample_frac(0.6)
+  
   # Create testing data - 40%
   test <- subset %>% 
     anti_join(., train, by = 'row_id')  %>%
@@ -26,17 +27,18 @@ for(i in unique(top_15_sig_importance$lvl2_ecoregion)) {
                           how_unbalanced$Human, how_unbalanced$Lightning)
   
   importance_subset <- top_15_sig_importance %>% 
-    filter(na_l2name == i ) %>%
+    filter(na_l2name == i) %>%
     droplevels() %>%
     mutate(variables = case_when(
       str_detect(variables, "season") ~ "seasons",
       str_detect(variables, "state") ~ "state",
+      str_detect(variables, "owner_descr") ~ "owner_descr",
       TRUE ~ .$variables))  
   
   importance_val_list <- as.vector(importance_subset$variables)
   importance_val_list <- paste0("c('ignition', ", paste(shQuote(importance_val_list), collapse=", "), ')')
 
-  if(!file.exists(file.path(model_dir, 'cart_', i, '.rds'))) {
+  if(!file.exists(file.path(rpart_model_dir, paste0('rpart_', gsub(' ', '-', i), '.rds')))) {
     training_parameters <- trainControl(method = "repeatedcv",
                                         number = 10,
                                         repeats = 15,
@@ -51,13 +53,16 @@ for(i in unique(top_15_sig_importance$lvl2_ecoregion)) {
                                 weights = model_weights,
                                 tuneLength = 5, 
                                 trControl = training_parameters)
-    write_rds(model_rpart, file.path(model_dir, 'cart_', i, '.rds'))
+    write_rds(model_rpart, file.path(rpart_model_dir, paste0('rpart_', gsub(' ', '-', i), '.rds')))
     
     predict_rpart <- predict(model_rpart, test)
-    write_rds(predict_rpart, file.path(model_dir, 'cart_prediction_', i, '.rds'))
+    write_rds(predict_rpart, file.path(rpart_model_dir, paste0('rpart_prediction_', gsub(' ', '-', i), '.rds')))
     
     confusion_rpar <- confusionMatrix(predict_rpart, test$ignition)
-    write_rds(confusion_rpar, file.path(model_dir, 'cart_confusion_', i, '.rds'))
+    write_rds(confusion_rpar, file.path(rpart_model_dir, paste0('rpart_confusion_', gsub(' ', '-', i), '.rds')))
+    
+    system(paste0('aws s3 sync ', model_dir, ' ', s3_proc_models, ' --delete'))
+    
     }
 }
 
