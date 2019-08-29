57e3fd846c9edb7e6461cd5f2569b775a445f20e
‘NateMietk’ <natemietkiewicz@gmail.com>
2019-08-29 18:42:13 +0000

    auc was broken - added a dataframe that now pulls from geom_roc auc outputs.

--- src/R/importance_vals_plots.R
+++ src/R/importance_vals_plots.R
@@ -35,49 +35,59 @@ preds<- do.call(rbind,lapply(mod_files,
                   as_tibble()
                 return(df)}))
 
-auc_values <- preds %>%
-  group_by(na_l2name) %>%
-  summarize(AUC = auc(obs,Human))
-
 # Plot the ROC evaluation of the model 
 model_ranger_roc_h <- preds %>%
   ggplot(aes(m = Human, d = ifelse(obs == 'Human', 1, 0))) + 
   geom_roc(n.cuts=0) + 
   facet_wrap(~ na_l2name) 
 
+roc_values <- calc_auc(model_ranger_roc_h)
+
 model_ranger_roc <- preds %>%
   ggplot() + 
   geom_roc(aes(m = Human, d = ifelse(obs == 'Human', 1, 0)), n.cuts=0, color = 'red') + 
-  geom_roc(aes(m = Lightning, d = ifelse(obs == 'Lightning', 1, 0)), n.cuts=0, color = 'royalblue') + 
+  # geom_roc(aes(m = Lightning, d = ifelse(obs == 'Lightning', 1, 0)), n.cuts=0, color = 'royalblue') + 
   coord_equal() +
   style_roc() +
   theme(strip.background = element_blank(),
         strip.text.x = element_text(size = 12)) +
   facet_wrap(~ na_l2name, labeller = label_wrap_gen(width = 15), ncol = 7) +
-  annotate("text", x=0.75, y=0.35, label=paste("AUC =", round(calc_auc(model_ranger_roc_h)$AUC, 3)))
+  annotate("text", x=0.75, y=0.35, label=paste("AUC =", round(roc_values$AUC, 3)))
 model_ranger_roc
 
+auc_values <- unique(ecoregions_l3$na_l2name) %>%
+  as.tibble() %>%
+  arrange(value) %>%
+  select(na_l2name = value) %>%
+  mutate(PANEL = as.factor(row_number())) %>%
+  left_join(., roc_values, by = 'PANEL') %>%
+  dplyr::select(na_l2name, AUC)
+
 ecoregion_auc <- ecoregions_l3 %>%
-  lwgeom::st_make_valid() %>%
+  sf::st_buffer(0) %>%
   group_by(na_l2name) %>%
   summarise(geometry = sf::st_union(geometry)) %>%
   ungroup() %>%
   left_join(., auc_values, by = 'na_l2name')
 
-ggplot(data = ecoregion_auc) +
+ecoregion_auc %>%
   sf::st_simplify(., preserveTopology = TRUE, dTolerance = 1000)  %>%
+  ggplot() +
   geom_sf(aes(fill = AUC)) +
   scale_fill_viridis_c(option = "plasma")
 
+regions <- as.data.frame(ecoregions_l3) %>%
+  distinct(na_l2name, region) %>%
+  as.tibble() 
 
 top_15_sig_importance <- importance_pval %>% 
   filter(variables != 'row_id') %>%
   filter(pvalue <= 0.05) %>%
   group_by(na_l2name) %>%
-  top_n(n = 15, wt = Overall) 
+  top_n(n = 10, wt = Overall) %>%
+  left_join(., regions, by = 'na_l2name')
 
 top_15_sig_importance %>% 
-  filter(na_l2name == 'Warm deserts') %>%
   ggplot(aes(x = reorder_within(variables, Overall, na_l2name), y = Overall)) + 
   facet_wrap(~ na_l2name, scales = 'free_y',
              nrow = 3) +
